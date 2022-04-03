5291f1b37b14a93f6655b15bbd3f69c62b114bdc
Артур <ostrikovartur21@gmail.com>
2022-04-03 14:30:31 +0300

    доробти

--- LuckyTicket/LuckyTicket/Program.cs
+++ LuckyTicket/LuckyTicket/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Hello, say your numbers on ticket");
+string ticket = Console.ReadLine ();
+Console.WriteLine($"your numbers is {ticket}");
+if (ticket.Length < 4)
+{
+    Console.WriteLine("string < 4");
+}
+else if (ticket.Length > 8)
+{
+    Console.WriteLine("string > 8");
+}
+ticket.Length /   
+
+
---
+++ ticket/ticket/Program.cs
@@ -0,0 +1,8 @@
+﻿string x = Console.ReadLine();
+{ 
+if (x[0] + x[1] + x[2] == x[3] + x[4] + x[5]) 
+    Console.WriteLine("Happy ticket");
+else
+        Console.WriteLine("Usual ticket");
+} 
+
