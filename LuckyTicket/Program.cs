834262ad9f08c3cdd491b239a3640e6724b45487
ostrikovartur <102900153+ostrikovartur@users.noreply.github.com>
2022-04-17 14:18:20 +0300

    Update Program.cs

--- LuckyTicket/Program.cs
+++ LuckyTicket/Program.cs
@@ -1,26 +1,47 @@
-﻿Console.WriteLine ("Hello, say your numbers on ticket");
-string ticket = Console.ReadLine ();
-Console.WriteLine($"your numbers is {ticket}");
-if (ticket.Length < 4)
+do
 {
-    Console.WriteLine("string < 4");
-}
-else if (ticket.Length > 8)
-{
-    Console.WriteLine("string > 8");
-}
-int x = ticket.Length;
+    Console.WriteLine("Hello, say your numbers on ticket");
+    string b = Console.ReadLine();
+    Console.WriteLine($"your numbers is {b}");
+    if (b.Length < 4)
+    {
+        Console.WriteLine("string < 4");
+    }
+    else if (b.Length > 8)
+    {
+        Console.WriteLine("string > 8");
+    }
+    else
+    {
+        int x = b.Length;
+        if (x != 0)
+        {
+            b = 0 + b;
+        }
+        string c = b.Substring(0, b.Length / 2);
+        string d = b.Substring(b.Length / 2);
+        int sum1 = 0;
+        int sum2 = 0;
 
-if (x%2 == 0)
-{
-    Console.WriteLine("Четное");
-}
-else
-{
-    Console.WriteLine("Не четное");
-    ticket = "0" + ticket;
-    
-    
-}
-
-
+        for (int i = 0; i < c.Length; i++)
+        {
+            if (char.IsDigit(c[i]))
+            {
+                sum1 += Convert.ToInt32(c[i].ToString());
+            }
+            if (char.IsDigit(d[i]))
+            {
+                sum2 += Convert.ToInt32(d[i].ToString());
+            }
+        }
+        if (sum1 == sum2)
+        {
+            Console.WriteLine("You have good luck!");
+        }
+        else
+        {
+            Console.WriteLine("Good luck another time!");
+        }
+    }
+    Console.WriteLine("Press Esc to get out or press any button to continue");
+} while (Console.ReadKey().Key != ConsoleKey.Escape);
